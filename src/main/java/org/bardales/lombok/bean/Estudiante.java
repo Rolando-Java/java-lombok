ccef08c8962f81aca70b012a07dba1bf261a7261
rolandoandre123 <rbardalespriale@gmail.com>
2021-10-10 21:35:43 -0500

    fix : modificando documentancion data
    Se modifico la documentacion acerca de la anotacion data de lombok
    Issue : DT-14

--- src/main/java/org/bardales/lombok/bean/Estudiante.java
+++ src/main/java/org/bardales/lombok/bean/Estudiante.java
@@ -8,6 +8,11 @@ import java.io.Serializable;
 //esta anotacion reune las caracteristicas
 //de la antacion @ToString, @Getter, @Setter
 //@RequiredArgsConstructor y @EqualsAndHashCode
+/*
+Sin embargo, el @RequiredArgsCinstructor solo se
+aplicara siempre y cuando no exista otro constructor
+en esta clase
+ */
 @Data
 public class Estudiante implements Serializable {
 
